11e9716217b09a6ebf56820e87217c1a77cffed4
Andrei Tarasevich <eansable@yandex.by>
2020-10-28 17:11:36 +0300

    feat: relized exptended repeater function

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,22 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options = {repeatTimes: 1, separator: '+', addition: '', additionRepeatTimes: 0, additionSeparator: ''}) {
+  if (options.separator === undefined) {
+    options.separator = '+';
+  }
+  if (options.addition !== undefined) {
+    str += `${options.addition}`
+  }
+  if (options.addition !== undefined && options.additionSeparator !== undefined) {
+    for (let i = 1; i < options.additionRepeatTimes; i++) {
+    str +=`${options.additionSeparator}${options.addition}`
+    }
+  }
+
+
+  let strResult = str;
+  for (let i = 1; i < options.repeatTimes; i++) {
+    strResult += `${options.separator}${str}`;
+  }
+  return strResult;
 };
-  

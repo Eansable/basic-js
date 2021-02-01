de292c7b4a38cf0e0e43ba065a9aa2c82d775567
Andrei Tarasevich <eansable@yandex.by>
2021-02-01 23:28:02 +0300

    feat: chain maiker is completed

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,25 +1,48 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+
+  arrayResult: [],
+
   getLength() {
-    throw new CustomError('Not implemented');
+    return this.arrayResult.length;
     // remove line with error and write your code here
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
+    this.arrayResult.push(` ${value} `);
+    return this;
     // remove line with error and write your code here
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
+    if ( typeof position === 'number') {
+      if (position < 1 || position > this.arrayResult.length){
+        this._deleteChain();
+        throw new Error('Removing link position is out for range')
+      }
+      this.arrayResult.splice(position - 1, 1);
+      return this;
+    }
+    this._deleteChain();
+    throw new Error('Position incorrect!');
+
     // remove line with error and write your code here
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
+    this.arrayResult.reverse();
+    return this;
     // remove line with error and write your code here
   },
   finishChain() {
-    throw new CustomError('Not implemented');
+    result = `(${this.arrayResult[0]})`
+    for (let i = 1; i < this.arrayResult.length; i++) {
+      result += `~~(${this.arrayResult[i]})`;
+    }
+    this._deleteChain();
+    return result;
     // remove line with error and write your code here
+  },
+  _deleteChain() {
+    this.arrayResult.length = 0;
   }
 };
 
